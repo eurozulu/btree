e280aa908b755bbd42d3a57713718b0b0492d7cd
Rob Gilham <rob.gilham@loomispay.com>
2024-07-07 14:56:18 +0200

    update gitignore

--- btree_test.go
+++ btree_test.go
@@ -3,6 +3,7 @@ package btree
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 	"testing"
@@ -93,4 +94,42 @@ func testGet(tree BTree[int, string], key int) error {
 		return fmt.Errorf("Get expected %s value for key %v, got %s", expect, key, s)
 	}
 	return nil
+}
+
+func TestPerformance(t *testing.T) {
+	TreeDegree = 100
+	TestSize := 10000000
+
+	ts := time.Now()
+	tree := NewTree[int, string]().(*btree[int, string])
+	for i := 0; i < TestSize; i++ {
+		tree.Put(i, "-"+strconv.Itoa(i)+"-")
+	}
+	t.Logf("Took %v to load btree with %d items", time.Since(ts), TestSize)
+	ts = time.Now()
+	for i := 0; i < TestSize; i++ {
+		k := rand.Intn(TestSize)
+		_, ok := tree.Get(k)
+		if !ok {
+			t.Errorf("Failed to find key %d in tree", k)
+		}
+	}
+	t.Logf("Took %v to search %d items in btree", time.Since(ts), TestSize)
+
+	ts = time.Now()
+	m := map[int]string{}
+	for i := 0; i < TestSize; i++ {
+		m[i] = "-" + strconv.Itoa(i) + "-"
+	}
+	t.Logf("Took %v to load hash map with %d items", time.Since(ts), TestSize)
+	ts = time.Now()
+	for i := 0; i < TestSize; i++ {
+		k := rand.Intn(TestSize)
+		_, ok := m[i]
+		if !ok {
+			t.Errorf("Failed to find key %d in map", k)
+		}
+	}
+	t.Logf("Took %v to search %d items in map", time.Since(ts), TestSize)
+
 }
